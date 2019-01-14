fdf06db54534296e9d00dc58d247284b2ffc716d
Razvan Becheriu <razvan.becheriu@qualitance.com>
2019-01-14 13:01:13 +0200

    fixed compilation

--- src/lib/dhcpsrv/benchmarks/run_benchmarks.cc
+++ src/lib/dhcpsrv/benchmarks/run_benchmarks.cc
@@ -1,4 +1,4 @@
-// Copyright (C) 2017 Deutsche Telekom AG.
+// Copyright (C) 2017-2018 Deutsche Telekom AG.
 //
 // Authors: Andrei Pavel <andrei.pavel@qualitance.com>
 //
@@ -20,8 +20,7 @@
 #include <log/logger_support.h>
 
 /// @brief A simple class that initializes logging.
-class Initializer {
-public:
+struct Initializer {
     Initializer() {
         isc::log::initLogger();
     }
@@ -29,5 +28,4 @@ public:
 
 Initializer initializer;
 
-BENCHMARK_MAIN()
-
+BENCHMARK_MAIN();
